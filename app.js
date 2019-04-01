d6e49c84c856cf5ae13d6a7472f6c7b7693e2288
lamar <liliulun@gmail.com>
2019-04-01 16:04:03 -0700

    added basic user authentication

--- app.js
+++ app.js
@@ -17,6 +17,17 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 //seedDB();
 
+//PASSPORT CONFIG
+app.use(require("express-session")({
+	secret: "1nniH1VAznUoCddAgKB1",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //LANDING PAGE
 app.get("/", function(req,res){
@@ -103,6 +114,27 @@ app.post("/recipes/:id/comments", function(req,res){
 
 			});
 		}
+	});
+});
+
+//AUTH ROUTES
+
+//show register form
+app.get("/register", function(req,res){
+	res.render("register");
+});
+
+//handle sign up logic
+app.post("/register", function(req,res){
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, function(err, user){
+		if(err){
+			console.log(err);
+			return res.render("register");
+		}
+		passport.authenticate("local")(req,res,function(){
+			res.redirect("/recipes");
+		});
 	});
 });
 
