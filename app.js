d43c445ddf86fc66e62b22ee371959cd1bf573ca
lamar <liliulun@gmail.com>
2019-04-01 15:49:12 -0700

    added user model

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express"),
       app     = express(),
 	  bodyParser = require("body-parser"),
 	  mongoose = require("mongoose"),
+	  passport = require("passport"),
+	  LocalStrategy = require("passport-local"),
 	  Recipe   = require("./models/recipe"),
 	  Comment   = require("./models/comment"),
+	  User      = require("./models/user"),
 	  seedDB   = require("./seeds");
 	  port = 3000;
 
