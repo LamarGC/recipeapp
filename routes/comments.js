ad6b733dda716eda14d8dbcbb3055801c40f5086
lamar <liliulun@gmail.com>
2019-04-01 19:31:49 -0700

    comment destroy

--- routes/comments.js
+++ routes/comments.js
@@ -71,6 +71,18 @@ router.put("/:comment_id", function(req, res){
 	});
 })
 
+//comment destroy
+router.delete("/:comment_id", function(req, res){
+	Comment.findByIdAndRemove(req.params.comment_id, function(err){
+		if(err){
+			res.redirect("back");
+		}
+		else{
+			res.redirect("/recipes/" + req.params.id);
+		}
+	});
+});
+
 
 function isLoggedIn(req, res, next){
 	if(req.isAuthenticated()){
