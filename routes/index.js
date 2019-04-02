2034a21c12e07752ece74070f5c2d69ddba28d1b
lamar <liliulun@gmail.com>
2019-04-01 20:27:55 -0700

    flash for error and success

--- routes/index.js
+++ routes/index.js
@@ -45,6 +45,7 @@ router.post("/login", passport.authenticate("local",
 //logout
 router.get("/logout", function(req,res){
 	req.logout();
+	req.flash("success", "Logged you out");
 	res.redirect("/recipes");
 });
 
