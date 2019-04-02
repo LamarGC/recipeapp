2aff130a31a702d1586a91bb6a1e906f2edc227e
lamar <liliulun@gmail.com>
2019-04-01 19:16:20 -0700

    comment edit and update

--- routes/comments.js
+++ routes/comments.js
@@ -46,6 +46,32 @@ router.post("/", isLoggedIn, function(req,res){
 	});
 });
 
+
+//comment edit route
+router.get("/:comment_id/edit", function(req,res){
+	Comment.findById(req.params.comment_id, function(err, foundComment){
+		if(err){
+			res.redirect("back");
+		}
+		else{
+			res.render("comments/edit", {recipe_id: req.params.id, comment: foundComment});
+		}
+	});
+});
+
+//comment update
+router.put("/:comment_id", function(req, res){
+	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+		if(err){
+			res.redirect("back");
+		}
+		else{
+			res.redirect("/recipes/" + req.params.id);
+		}
+	});
+})
+
+
 function isLoggedIn(req, res, next){
 	if(req.isAuthenticated()){
 		return next();
